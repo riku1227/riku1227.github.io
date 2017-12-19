a75f9ab741b8a5e1d0eb7269f3c453d911e9619a
riku1227 <rikum20021227@gmail.com>
2017-12-19 23:57:03 +0900

    Add ripple effect

--- materialy/js/main.js
+++ materialy/js/main.js
@@ -4,6 +4,13 @@
     function setupHeader(html) {
         document.getElementsByClassName('header')[0].innerHTML = html;
     }
+    
+    [].forEach.call(document.getElementsByClassName("button"),function(element){
+        element.addEventListener("click",rippleAnimation);
+    });
+    [].forEach.call(document.getElementsByClassName("card__button"),function(element){
+        element.addEventListener("click",rippleAnimation);
+    });
 }());
 
 function loadFile(url,callback) {
@@ -14,4 +21,22 @@ function loadFile(url,callback) {
         this.callback.call(this,request.responseText);
     }
     request.send(null);
+}
+
+function rippleAnimation(e) {
+    var button = this;
+    var spanCover = document.createElement("span");
+    var spanSize = button.offsetWidth;
+    var location = button.getBoundingClientRect();
+    var x = e.pageX - location.left - window.pageXOffset - (spanSize / 2);
+    var y = e.pageY - location.top - window.pageYOffset - (spanSize / 2);
+    var position = "top:" + y + "px; left:" + x + "px; height:" + spanSize + "px; width:" + spanSize + "px;";
+    button.appendChild(spanCover);
+    spanCover.setAttribute("style", position);
+    spanCover.className = this.className + "__ripple";
+    setTimeout(function() {
+            var list = document.getElementsByClassName(this.className + "__ripple");
+            for(var i =list.length-1;i>=0; i--){
+            list[i].parentNode.removeChild(list[i]);
+    }}, 350)
 }
