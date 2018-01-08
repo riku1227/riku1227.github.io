a912a264b739c619155e0de6ca54edcfaab3e165
riku1227 <rikum20021227@gmail.com>
2018-01-08 17:59:46 +0900

    JavaScriptがいい感じになったかもしれない

--- js/distributions/index.js
+++ js/distributions/index.js
@@ -1,9 +1,10 @@
+"use strict";
 (function () {
     loadFile("https://riku1227.github.io/json/distributions/index.json",generateCard);
 }());
 
 function loadFile(url,callback) {
-    var request = new XMLHttpRequest();
+    const request = new XMLHttpRequest();
     request.open("get", url, true);
     request.callback = callback
     request.onloadend = function() {
@@ -13,37 +14,37 @@ function loadFile(url,callback) {
 }
 
 function generateCard(json) {
-    var result = "";
-    var jsonObject = JSON.parse(json);
-    
-    for(var i = 0;i < jsonObject.card_list.length;i++) {
-        for(var j = jsonObject.card_list.length-1;j > i;j--) {
+    let result = "";
+    const jsonObject = JSON.parse(json);
+
+    for(let i = 0;i < jsonObject.card_list.length;i++) {
+        for(let j = jsonObject.card_list.length-1;j > i;j--) {
             if(jsonObject.card_list[j].data > jsonObject.card_list[j-1].data) {
-                var temp = jsonObject.card_list[j];
+                const temp = jsonObject.card_list[j];
                 jsonObject.card_list[j] = jsonObject.card_list[j-1];
                 jsonObject.card_list[j-1] = temp;
             }
         }
     }
-    
-    for (var i = 0;i < jsonObject.card_list.length;i++) {
-        var name = jsonObject.card_list[i].name;
-        var thumbnail = jsonObject.card_list[i].image_thumbnail;
-        var data = jsonObject.card_list[i].data;
-        var dataStr = jsonObject.card_list[i].dataStr;
-        var description = jsonObject.card_list[i].description;
-        var linkDL = jsonObject.card_list[i].link_dl;
-        var linkDetails = jsonObject.card_list[i].link_details;
-        var html = '<div class="cardList">'+
-        '<img class="cardList__image" src="../../image/distributions/'+thumbnail+'/cover_image.jpg">'+
-        '<p class="cardList__title">'+name+'</p>'+
-        '<p class="cardList__text">最終更新日: '+dataStr+'</p>'+
-        '<p class="cardList__text">'+description+'</p>'+
-        '<div class="center">'+
-        '<a class="cardList__button" href="'+linkDL+'">ダウンロード</a>'+
-        '<a class="cardList__button" href="'+linkDetails+'">詳細</a>'+
-        '</div>'+
-        '</div>';
+
+    for (let i = 0;i < jsonObject.card_list.length;i++) {
+        const name = jsonObject.card_list[i].name;
+        const thumbnail = jsonObject.card_list[i].image_thumbnail;
+        const data = jsonObject.card_list[i].data;
+        const dataStr = jsonObject.card_list[i].dataStr;
+        const description = jsonObject.card_list[i].description;
+        const linkDL = jsonObject.card_list[i].link_dl;
+        const linkDetails = jsonObject.card_list[i].link_details;
+        const html = '<div class="cardList">'+
+                     `<img class="cardList__image" src="../../image/distributions/${thumbnail}/cover_image.jpg">`+
+                     `<p class="cardList__title">${name}</p>`+
+                     `<p class="cardList__text">最終更新日: ${dataStr}</p>`+
+                     `<p class="cardList__text">${description}</p>`+
+                     '<div class="center">'+
+                     `<a class="cardList__button" href="${linkDL}">ダウンロード</a>`+
+                     `<a class="cardList__button" href="${linkDetails}">詳細</a>`+
+                     '</div>'+
+                     '</div>';
         result += html;
     }
     document.getElementsByClassName('center')[0].innerHTML=result;
--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+"use strict";
 (function () {
     loadFile("https://riku1227.github.io/html/core/header.html",setupHeader);
 
@@ -17,7 +18,7 @@
 }());
 
 function loadFile(url,callback) {
-    var request = new XMLHttpRequest();
+    const request = new XMLHttpRequest();
     request.open("get", url, true);
     request.callback = callback
     request.onloadend = function() {
@@ -27,19 +28,19 @@ function loadFile(url,callback) {
 }
 
 function rippleAnimation(e) {
-    var button = this;
-    var spanCover = document.createElement("span");
-    var spanSize = button.offsetWidth;
-    var location = button.getBoundingClientRect();
-    var x = e.pageX - location.left - window.pageXOffset - (spanSize / 2);
-    var y = e.pageY - location.top - window.pageYOffset - (spanSize / 2);
-    var position = "top:" + y + "px; left:" + x + "px; height:" + spanSize + "px; width:" + spanSize + "px;";
+    const button = this;
+    const spanCover = document.createElement("span");
+    const spanSize = button.offsetWidth;
+    const location = button.getBoundingClientRect();
+    const x = e.pageX - location.left - window.pageXOffset - (spanSize / 2);
+    const y = e.pageY - location.top - window.pageYOffset - (spanSize / 2);
+    const position = `top:${y}px; left:${x}px; height:${spanSize}px; width:${spanSize}px;`;
     button.appendChild(spanCover);
     spanCover.setAttribute("style", position);
-    spanCover.className = this.className + "__ripple";
+    spanCover.className = `${this.className}__ripple`;
     setTimeout(function() {
-            var list = document.getElementsByClassName(this.className + "__ripple");
-            for(var i =list.length-1;i>=0; i--){
+            const list = document.getElementsByClassName(`${this.className}__ripple`);
+            for(let i =list.length-1;i>=0; i--){
             list[i].parentNode.removeChild(list[i]);
     }}, 350)
 }
