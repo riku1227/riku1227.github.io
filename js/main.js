8cac7afb872d536f0bc4ebbabb93d4d5f2076a48
riku1227 <rikum20021227@gmail.com>
2018-01-20 13:26:11 +0900

    Fix bug

--- js/main.js
+++ js/main.js
@@ -15,32 +15,39 @@
     [].forEach.call(document.getElementsByClassName("cardList__button"),function(element){
         element.addEventListener("click",rippleAnimation);
     });
-}());
 
-function loadFile(url,callback) {
-    const request = new XMLHttpRequest();
-    request.open("get", url, true);
-    request.callback = callback
-    request.onloadend = function() {
-        this.callback.call(this,request.responseText);
+    function loadFile(url,callback) {
+        const request = new XMLHttpRequest();
+        request.open("get", url, true);
+        request.callback = callback
+        request.onloadend = function() {
+            this.callback.call(this,request.responseText);
+        }
+        request.send(null);
     }
-    request.send(null);
-}
 
-function rippleAnimation(e) {
-    const button = this;
-    const spanCover = document.createElement("span");
-    const spanSize = button.offsetWidth;
-    const location = button.getBoundingClientRect();
-    const x = e.pageX - location.left - window.pageXOffset - (spanSize / 2);
-    const y = e.pageY - location.top - window.pageYOffset - (spanSize / 2);
-    const position = `top:${y}px; left:${x}px; height:${spanSize}px; width:${spanSize}px;`;
-    button.appendChild(spanCover);
-    spanCover.setAttribute("style", position);
-    spanCover.className = `${this.className}__ripple`;
-    setTimeout(function() {
-            const list = document.getElementsByClassName(`${button.className}__ripple`);
-            for(let i =list.length-1;i>=0; i--){
-            list[i].parentNode.removeChild(list[i]);
-    }}, 2500);
-}
+    let rippleDelete = false;
+
+    function rippleAnimation(e) {
+        const button = this;
+        const spanCover = document.createElement("span");
+        const spanSize = button.offsetWidth;
+        const location = button.getBoundingClientRect();
+        const x = e.pageX - location.left - window.pageXOffset - (spanSize / 2);
+        const y = e.pageY - location.top - window.pageYOffset - (spanSize / 2);
+        const position = `top:${y}px; left:${x}px; height:${spanSize}px; width:${spanSize}px;`;
+        button.appendChild(spanCover);
+        spanCover.setAttribute("style", position);
+        spanCover.className = `${this.className}__ripple`;
+        if(!rippleDelete) {
+            rippleDelete = true;
+            setTimeout(function() {
+                const list = document.getElementsByClassName(`${button.className}__ripple`);
+                for(let i =list.length-1;i>=0; i--){
+                    list[i].parentNode.removeChild(list[i]);
+                }
+                rippleDelete = false;
+            }, 2500);
+        }
+    }
+}());
