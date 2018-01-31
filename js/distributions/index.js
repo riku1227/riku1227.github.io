3706efcb1863f41f265ad3d74fddc0c9cfd9b537
riku1227 <rikum20021227@gmail.com>
2018-01-31 19:23:56 +0900

    4space to 2space

--- js/distributions/index.js
+++ js/distributions/index.js
@@ -1,74 +1,74 @@
 "use strict";
 (function () {
-    loadFile("https://riku1227.github.io/json/distributions/index.json",generateCard);
+  loadFile("https://riku1227.github.io/json/distributions/index.json",generateCard);
 }());
 
 function loadFile(url,callback) {
-    const request = new XMLHttpRequest();
-    request.open("get", url, true);
-    request.callback = callback
-    request.onloadend = function() {
-        this.callback.call(this,request.responseText);
-    }
-    request.send(null);
+  const request = new XMLHttpRequest();
+  request.open("get", url, true);
+  request.callback = callback
+  request.onloadend = function() {
+    this.callback.call(this,request.responseText);
+  }
+  request.send(null);
 }
 
 function generateCard(json) {
-    let result = "";
-    const jsonObject = JSON.parse(json);
+  let result = "";
+  const jsonObject = JSON.parse(json);
 
-    function quickSort(startID, endID) {
-        let pivot = jsonObject.card_list[Math.floor((startID + endID)/2)].data;
-        let left = startID;
-        let right = endID;
+  function quickSort(startID, endID) {
+    let pivot = jsonObject.card_list[Math.floor((startID + endID)/2)].data;
+    let left = startID;
+    let right = endID;
 
-        while(true) {
-            while(jsonObject.card_list[left].data>pivot) {
-                left++;
-            }
-            while(pivot>jsonObject.card_list[right].data) {
-                right--;
-            }
-            if(right <= left) {
-                break;
-            }
-            
-            let tmp =jsonObject.card_list[left];
-            jsonObject.card_list[left] =jsonObject.card_list[right];
-            jsonObject.card_list[right] =tmp;
-            left++;
-            right--;
-        }
-
-        if(startID < left-1){
-            quickSort(startID,left-1);
-        }
-        if(right+1 < endID){
-            quickSort(right+1,endID);
-        }
+    while(true) {
+      while(jsonObject.card_list[left].data>pivot) {
+        left++;
+      }
+      while(pivot>jsonObject.card_list[right].data) {
+        right--;
+      }
+      if(right <= left) {
+        break;
+      }
+      
+      let tmp =jsonObject.card_list[left];
+      jsonObject.card_list[left] =jsonObject.card_list[right];
+      jsonObject.card_list[right] =tmp;
+      left++;
+      right--;
     }
 
-    quickSort(0,jsonObject.card_list.length-1);
-
-    for (let i = 0;i < jsonObject.card_list.length;i++) {
-        const name = jsonObject.card_list[i].name;
-        const thumbnail = jsonObject.card_list[i].image_thumbnail;
-        const data = jsonObject.card_list[i].data;
-        const dataStr = jsonObject.card_list[i].dataStr;
-        const description = jsonObject.card_list[i].description;
-        const linkDL = jsonObject.card_list[i].link_dl;
-        const linkDetails = jsonObject.card_list[i].link_details;
-        const html = '<div class="cardList">'+
-                     `<img class="cardList__image" src="../../image/distributions/${thumbnail}/cover_image.jpg">`+
-                     `<p class="cardList__title">${name}</p>`+
-                     `<p class="cardList__text">最終更新日: ${dataStr}</p>`+
-                     `<p class="cardList__text">${description}</p>`+
-                     '<div class="center">'+
-                     `<a class="cardList__button" href="${linkDL}">ダウンロード</a>`+
-                     `<a class="cardList__button" href="${linkDetails}">詳細</a>`+
-                     '</div>'+
-                     '</div>';
-        result += html;
+    if(startID < left-1){
+      quickSort(startID,left-1);
     }
-    document.getElementsByClassName('center')[0].innerHTML=result;
+    if(right+1 < endID){
+      quickSort(right+1,endID);
+    }
+  }
+
+  quickSort(0,jsonObject.card_list.length-1);
+
+  for (let i = 0;i < jsonObject.card_list.length;i++) {
+    const name = jsonObject.card_list[i].name;
+    const thumbnail = jsonObject.card_list[i].image_thumbnail;
+    const data = jsonObject.card_list[i].data;
+    const dataStr = jsonObject.card_list[i].dataStr;
+    const description = jsonObject.card_list[i].description;
+    const linkDL = jsonObject.card_list[i].link_dl;
+    const linkDetails = jsonObject.card_list[i].link_details;
+    const html = '<div class="cardList">'+
+           `<img class="cardList__image" src="../../image/distributions/${thumbnail}/cover_image.jpg">`+
+           `<p class="cardList__title">${name}</p>`+
+           `<p class="cardList__text">最終更新日: ${dataStr}</p>`+
+           `<p class="cardList__text">${description}</p>`+
+           '<div class="center">'+
+           `<a class="cardList__button" href="${linkDL}">ダウンロード</a>`+
+           `<a class="cardList__button" href="${linkDetails}">詳細</a>`+
+           '</div>'+
+           '</div>';
+    result += html;
+  }
+  document.getElementsByClassName('center')[0].innerHTML=result;
 }
--- js/main.js
+++ js/main.js
@@ -1,55 +1,55 @@
 "use strict";
 (function () {
-    loadFile("https://riku1227.github.io/html/core/header.html",setupHeader);
+  loadFile("https://riku1227.github.io/html/core/header.html",setupHeader);
 
-    function setupHeader(html) {
-        document.getElementsByClassName('header')[0].innerHTML = html;
+  function setupHeader(html) {
+    document.getElementsByClassName('header')[0].innerHTML = html;
+  }
+
+  document.getElementById("test").innerHTML = window.navigator.userAgent.toLowerCase();
+
+  [].forEach.call(document.getElementsByClassName("button"),function(element){
+    element.addEventListener("click",rippleAnimation);
+  });
+  [].forEach.call(document.getElementsByClassName("card__button"),function(element){
+    element.addEventListener("click",rippleAnimation);
+  });
+  [].forEach.call(document.getElementsByClassName("cardList__button"),function(element){
+    element.addEventListener("click",rippleAnimation);
+  });
+
+  function loadFile(url,callback) {
+    const request = new XMLHttpRequest();
+    request.open("get", url, true);
+    request.callback = callback
+    request.onloadend = function() {
+      this.callback.call(this,request.responseText);
     }
+    request.send(null);
+  }
 
-    document.getElementById("test").innerHTML = window.navigator.userAgent.toLowerCase();
+  let rippleDelete = false;
 
-    [].forEach.call(document.getElementsByClassName("button"),function(element){
-        element.addEventListener("click",rippleAnimation);
-    });
-    [].forEach.call(document.getElementsByClassName("card__button"),function(element){
-        element.addEventListener("click",rippleAnimation);
-    });
-    [].forEach.call(document.getElementsByClassName("cardList__button"),function(element){
-        element.addEventListener("click",rippleAnimation);
-    });
-
-    function loadFile(url,callback) {
-        const request = new XMLHttpRequest();
-        request.open("get", url, true);
-        request.callback = callback
-        request.onloadend = function() {
-            this.callback.call(this,request.responseText);
+  function rippleAnimation(e) {
+    const button = this;
+    const spanCover = document.createElement("span");
+    const spanSize = button.offsetWidth;
+    const location = button.getBoundingClientRect();
+    const x = e.pageX - location.left - window.pageXOffset - (spanSize / 2);
+    const y = e.pageY - location.top - window.pageYOffset - (spanSize / 2);
+    const position = `top:${y}px; left:${x}px; height:${spanSize}px; width:${spanSize}px;`;
+    button.appendChild(spanCover);
+    spanCover.setAttribute("style", position);
+    spanCover.className = `${this.className}__ripple`;
+    if(!rippleDelete) {
+      rippleDelete = true;
+      setTimeout(function() {
+        const list = document.getElementsByClassName(`${button.className}__ripple`);
+        for(let i =list.length-1;i>=0; i--){
+          list[i].parentNode.removeChild(list[i]);
         }
-        request.send(null);
+        rippleDelete = false;
+      }, 2500);
     }
-
-    let rippleDelete = false;
-
-    function rippleAnimation(e) {
-        const button = this;
-        const spanCover = document.createElement("span");
-        const spanSize = button.offsetWidth;
-        const location = button.getBoundingClientRect();
-        const x = e.pageX - location.left - window.pageXOffset - (spanSize / 2);
-        const y = e.pageY - location.top - window.pageYOffset - (spanSize / 2);
-        const position = `top:${y}px; left:${x}px; height:${spanSize}px; width:${spanSize}px;`;
-        button.appendChild(spanCover);
-        spanCover.setAttribute("style", position);
-        spanCover.className = `${this.className}__ripple`;
-        if(!rippleDelete) {
-            rippleDelete = true;
-            setTimeout(function() {
-                const list = document.getElementsByClassName(`${button.className}__ripple`);
-                for(let i =list.length-1;i>=0; i--){
-                    list[i].parentNode.removeChild(list[i]);
-                }
-                rippleDelete = false;
-            }, 2500);
-        }
-    }
+  }
 }());
