cfc10cc7569e38e427740f248bf58b1267f24906
riku1227 <rikum20021227@gmail.com>
2018-04-29 21:24:56 +0900

    内容をjsonから生成する機能を追加

--- js/distributions/index.js
+++ js/distributions/index.js
@@ -69,6 +69,89 @@ const generateCard = function(json) {
   document.getElementsByClassName('center')[0].innerHTML=result;
 };
 
+let urlName = "";
+
+const generateContent = function(json) {
+  try {
+    let jsonObject = JSON.parse(json);
+    document.title = jsonObject.information.name + " - riku1227 Website";
+    let headArray = document.head.children;
+    headArray[10].content = jsonObject.information.name + " - riku1227 Website";
+    headArray[11].content = location.href;
+    headArray[12].content = "https://riku1227.github.io/image/distributions/" + urlName + "/cover_image.jpg";
+    headArray[13].content = jsonObject.information.description;
+
+    document.getElementsByClassName("center")[0].className = "materialy-mainContent";
+    let htmlResult = "";
+    const titleTagName = "materialy-card__title";
+    const subTitleName = "materialy-card__subtitle";
+    const textTag = "materialy-card__text";
+    const linkTagName = "materialy-button--flat--accent";
+    const imageTagName = "materialy-card__image";
+
+    let infoCard = `<div class="materialy-card">`+
+    `<div class="materialy-card__image--title"> <img src="../../image/distributions/${urlName}/cover_image.jpg"> </div>`+
+    `<p class="${titleTagName}">${jsonObject.information.name}</p>`+
+    `<p class="${textTag}">${jsonObject.information.description}</p>`+
+    `</div>`;
+    htmlResult += infoCard;
+
+    let mode = 0;
+    let tempStr = 0;
+
+    for(let i = 0; i != jsonObject.cards.length; i++) {
+      let cardObject = jsonObject.cards[i];
+
+      let cardHtml = `<div class="materialy-card">`+
+      `<p class="${titleTagName}">${cardObject.title}</p>`;
+
+      for(let j = 0; j != cardObject.content.length; j++) {
+        let contentStr = cardObject.content[j];
+        if(contentStr.indexOf("$") != -1) {
+          let str = contentStr.substring(1);
+          let variableName = str.substring(0, str.indexOf("("));
+          let variableContet = str.substring(str.indexOf("(") + 1, str.indexOf(")"));
+          switch(variableName) {
+            case "title":
+              cardHtml += `<p class="${titleTagName}">${variableContet}</p>`;
+            break;
+            case "subTitle":
+              cardHtml += `<p class="${subTitleName}">${variableContet}</p>`;
+            break;
+            case "img":
+              cardHtml += `<div class="${imageTagName}"><img src="../../image/distributions/${urlName}/${variableContet}"></div>`;
+            break;
+            case "link":
+              let linkArray = variableContet.split(",");
+              cardHtml += `<a class="${linkTagName}" href="${linkArray[1]}">${linkArray[0]}</a>`;
+            break;
+          }
+        } else {
+          cardHtml += `<p class="${textTag}">${contentStr}</p>`;
+        }
+      }
+
+      cardHtml += `</div>`;
+      htmlResult += cardHtml;
+    }
+    document.getElementsByClassName("materialy-mainContent")[0].innerHTML = htmlResult;
+  } catch (e) {
+    document.getElementsByClassName("materialy-mainContent")[0].innerHTML = "お探しのページは存在しません";
+    console.log(e);
+  }
+};
+
 (function () {
-  loadFile("https://riku1227.github.io/json/distributions/index.json",generateCard);
+  let urlParameter = location.search.substring(1);
+  if(urlParameter === "") {
+    loadFile("https://riku1227.github.io/json/distributions/index.json",generateCard);
+  } else {
+    let parameter = urlParameter.split("&")[0].split("=");
+    if(parameter[0] === "item") {
+      urlName = parameter[1];
+      loadFile(`https://riku1227.github.io/json/distributions/items/${parameter[1]}.json`,generateContent);
+    } else {
+      loadFile("https://riku1227.github.io/json/distributions/index.json",generateCard);
+    }
+  }
 }());
